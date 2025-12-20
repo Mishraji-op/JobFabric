6718ff5915ef204bbf9b3d2937c4ac2592cdaa99
Raj Mishra <rajmishra@Mishraji-mac.local>
2025-12-20 21:59:49 +0530

    Initial commit

---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white'>
+<div>
+  <h1 className='text-2xl font-bold'>Job<span className='text-[#3b82f6]'>Fabric</span></h1>
+</div>
+<div>
+
+</div>
+
+    </div>
+  )
+}
+
+export default Navbar
