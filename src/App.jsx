0d81780fefc3dc214b343556837bb5e4f3ccd1f4
Raj Mishra <rajmishra@Mishraji-mac.local>
2025-12-28 19:05:24 +0530

    updated the browse section and updated the jobs and filter card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import Home from "./components/Home";
 import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
 
 
 
@@ -25,6 +26,10 @@ const appRouter = createBrowserRouter([
     path: "/jobs",
     element: <Jobs/>,
   },
+  {
+    path:"/browse",
+    element:<Browse/>
+  }
   
 ]);
 function App() {
